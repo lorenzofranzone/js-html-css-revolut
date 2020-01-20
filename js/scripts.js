04cd3cbb757ca54d9ebc37d80390a5b2ae4b321f
Lorenzo <lorenzo.franzone@gmail.com>
2020-01-20 17:30:44 +0100

    JQuery Submenu

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,17 @@ $(document).ready(function(){
 
   // Code
 
-
+  // Identifico il link contenente un sotto menu
+  $('ul.menu-inline > li > a.sub').mouseenter(
+    function() {
+      $(this).next().show();
+    }
+  );
+  $('ul.menu-inline > li > a.sub').mouseleave(
+    function() {
+      $(this).next().hide();
+    }
+  );
 
 /////
 }); // Document Ready
